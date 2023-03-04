9c4eb9f044c886dd6db8e8dcb123162346ad56c2
emran <emranlateef786@gmail.com>
2023-03-04 18:17:59 +0500

    worked on dashboard welcome card and profile card

--- src/pages/User/Dashboard/Components/UserProfile.js
+++ src/pages/User/Dashboard/Components/UserProfile.js
@@ -1,8 +1,10 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { USER_STORAGE_KEY } from '../../../../config/helpers/variables'
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 const UserProfile = () => {
+  const user = JSON.parse(sessionStorage.getItem(USER_STORAGE_KEY));
   return (
     <section className='dashboard__container__content__userProfile'>
       <header className='dashboard__container__content__userProfile__header'>
@@ -14,19 +16,19 @@ const UserProfile = () => {
       </header>
       <div className='dashboard__container__content__userProfile__details'>
         <div className='dashboard__container__content__userProfile__details__detailsBox'>
-          <p>Broker Company: XXXX</p>
-          <p>Address: XXXX</p>
+          <p>Broker Company: {user?.data?.company_name ? user?.data?.company_name : "NiL"}</p>
+          <p>Address: {user?.data?.company_address ? user?.data?.company_address : "NiL"}</p>
         </div>
         <div className='dashboard__container__content__userProfile__details__detailsBox'>
-          <p>First Name: XXXX</p>
-          <p>Last Name: XXXX</p>
+          <p>First Name: {user?.data?.first_name ? user?.data?.first_name : "NiL"}</p>
+          <p>Last Name: {user?.data?.last_name ? user?.data?.last_name : "NiL"}</p>
         </div>
         <div className='dashboard__container__content__userProfile__details__detailsBox'>
-          <p>Email Address: XXXX</p>
-          <p>Phone Number: XXXX</p>
+          <p>Email Address: {user?.data?.email ? user?.data?.email : "NiL"}</p>
+          <p>Phone Number: {user?.data?.phone_number ? user?.data?.phone_number : "NiL"}</p>
         </div>
         <div className='dashboard__container__content__userProfile__details__detailsBox'>
-          <p>Reg. No: XXXX</p>
+          <p>Reg. No: {user?.data?.id}</p>
         </div>
       </div>
     </section>
--- src/pages/User/Dashboard/Components/Welcome.js
+++ src/pages/User/Dashboard/Components/Welcome.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { USER_STORAGE_KEY } from '../../../../config/helpers/variables'
 
 const Welcome = () => {
+
+  const user = JSON.parse(sessionStorage.getItem(USER_STORAGE_KEY));
+  if(user?.data?.first_name && user?.data?.last_name){
+    var name = `${user?.data?.first_name}` + ' ' + `${user?.data?.last_name}`
+  }
+  else{
+    var name = ""
+  }
+  
   return (
     <section className='dashboard__container__content__welcomePage'>
-      <h1 className='dashboard__container__content__welcomePage__heading'>Welcome back, XXXX!</h1>
-      <p className='dashboard__container__content__welcomePage__description'>Cavitas broken dashboard is for ...</p>
+      <h1 className='dashboard__container__content__welcomePage__heading'>Welcome back, {name}!</h1>
+      <p className='dashboard__container__content__welcomePage__description'>Cavitas broker dashboard is for ...</p>
       <p className='dashboard__container__content__welcomePage__support'>Need some help? Let's Chat.</p>
       <p className='dashboard__container__content__welcomePage__contact'>XXXXXXX &nbsp; | &nbsp;  support@cavitas.eu</p>
     </section>
