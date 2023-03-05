16bc5d40c3f66f193c018ac9fe6db27b6f1eaf41
abuzarbhatti46 <abuzarbhatti46@gmail.com>
2023-03-05 18:54:11 +0500

    design completed

--- src/pages/User/Claims/index.js
+++ src/pages/User/Claims/index.js
@@ -1,13 +1,37 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Page from '../../../components/Page/Page'
-import ClaimsPortal from './Components/ClaimsPortal'
 
 const Claims = () => {
   return (
     <Page>
       <section className='claims'>
         <div className='claims__container'>
-          <ClaimsPortal />
+          <section className='claims__container__claimsPortal'>
+            <header>
+              <h1>Welcome to the dental claims portal</h1>
+              <p>If you already registered on this portal, simply login with your <b>email address</b> and your <b> password. Please note that you must belong to an existing dental insurance Plan in order to login.</b></p>
+            </header>
+            <div className='claims__container__claimsPortal__loginBox'>
+              <div className='claims__container__claimsPortal__loginBox__details'>
+                <p>Login details</p>
+                <input type="email" />
+                <input type="password" />
+                <div>
+                  <Button>Login</Button>
+                </div>
+                <div className='claims__container__claimsPortal__loginBox__details__links'>
+                  <p>Click here <Link to="/password/forgot">Forgot your password?</Link></p>
+                  <p>Not yet registered? <Link to="/signup">Create an account</Link></p>
+                  <p>Trouble logging in? <Link to="/support">Contact support</Link></p>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div className='claims__container__picture'>
+            <img src={require("../../../assets/Claim-image.png")} alt="" />
+          </div>
         </div>
       </section>
     </Page>
