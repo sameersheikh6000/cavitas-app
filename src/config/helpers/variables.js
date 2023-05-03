b441595e2e99ff2ca011a061ef1c4c30b4228f14
sameersheikh6000@gmail.com <chalisherali@gmail.com>
2023-05-03 23:39:03 +0500

    Api keys change

--- src/config/helpers/variables.js
+++ src/config/helpers/variables.js
@@ -1,9 +1,9 @@
-export const API_KEY = "https://cavitas.herokuapp.com";
-export const FRONT_API_KEY = "https://cavitas-app.herokuapp.com";
+// export const API_KEY = "https://cavitas.herokuapp.com";
+// export const FRONT_API_KEY = "https://cavitas-app.herokuapp.com";
 
 
-// export const API_KEY = "http://localhost:3001";
-// export const FRONT_API_KEY = "http://localhost:3000";
+export const API_KEY = "http://localhost:3001";
+export const FRONT_API_KEY = "http://localhost:3000";
 
 export const USER_STORAGE_KEY = "CAVITAS_USER";
 export const ADMIN_STORAGE_KEY = "CAVITAS_ADMIN";
