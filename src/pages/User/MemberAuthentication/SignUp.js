01f749306faa2d8e4b1bd399c1ed5ad977192853
sameersheikh6000@gmail.com <chalisherali@gmail.com>
2023-07-20 17:03:06 +0500

    return issue fix

--- src/pages/User/MemberAuthentication/SignUp.js
+++ src/pages/User/MemberAuthentication/SignUp.js
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import { useTranslation } from "react-i18next";
-import i18n from "../../../config/helpers/i18n";
-import useUsers from "../../../hooks/useUsers";
-import AlertMessage from "../../../components/SnackbarMessages/AlertMessage";
-import { useNavigate } from "react-router-dom";
+import { Button } from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import i18n from '../../../config/helpers/i18n';
+import useUsers from '../../../hooks/useUsers';
+import AlertMessage from '../../../components/SnackbarMessages/AlertMessage';
+import { useNavigate } from 'react-router-dom';
 
 const MemberSignUp = () => {
   const currentUrl = window.location.href;
@@ -17,24 +17,25 @@ const MemberSignUp = () => {
     first_name: "",
     last_name: "",
     email: "",
-    company_pesel_number: "",
+    company_pesel_number: '',
     password: "",
     password_confirmation: "",
   });
 
-  const changeHandler = (event) => {
+  const changeHandler = event => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
   };
 
-  const submitHandler = async (e) => {
+  const submitHandler = async e => {
+
     e.preventDefault();
     for (let prop in user) {
       if (!user[prop]) return alert(t("Pannel_Dashboard_Supporttickets.fill"));
     }
     const response = await createUser(user, "member");
     if (response?.status?.code < 300) {
-      navigate(`/dashboard/${lang === "pl" ? lang : "en"}`);
+      navigate(`/dashboard/${lang === 'pl' ? lang : "en" }`);
     } else if (response?.data?.message !== undefined) {
       setErrorMessage(response?.data?.message);
       setTimeout(() => {
@@ -52,45 +53,33 @@ const MemberSignUp = () => {
     const currentUrl = window.location.href;
     let lang = currentUrl.split("/").pop();
     lang && i18n.changeLanguage(lang === "pl" ? lang : "en");
-  }, []);
+  }, [])
 
   return (
-    <section className="authentication">
+    <section className='authentication'>
       <AlertMessage errorMessage={errorMessage} />
-      <div className="authentication__container">
-        <div className="authentication__container__imageBox">
-          <div className="authentication__container__imageBox__top">
-            <img
-              className="authentication__container__imageBox__top__logo"
-              onClick={() => navigate(`/${lang === "pl" ? "pl" : "en"}`)}
-              src={require("../../../assets/Signin-logo.png")}
-              alt=""
-            />
+      <div className='authentication__container'>
+
+        <div className='authentication__container__imageBox'>
+          <div className='authentication__container__imageBox__top' >
+            <img className='authentication__container__imageBox__top__logo' onClick={() => navigate("/")}
+             src={require("../../../assets/Signin-logo.png")} alt='' />
             {/* <img className='authentication__container__imageBox__top__flag' src={require("../../../assets/Signin-flag.png")} alt='' /> */}
           </div>
 
-          <div className="authentication__container__imageBox__bottom">
-            <img
-              className="authentication__container__imageBox__bottom"
-              src={require("../../../assets/MemberLogin-image.png")}
-              alt=""
-            />
+          <div className='authentication__container__imageBox__bottom'>
+            <img className='authentication__container__imageBox__bottom' src={require("../../../assets/MemberLogin-image.png")} alt='' />
           </div>
         </div>
 
-        <div className="authentication__container__formContainer">
-          <p className="authentication__container__formContainer__registerHeading">
-            {t("Pannel_registration.letsregister")}
-          </p>
-          <form
-            onSubmit={submitHandler}
-            className="authentication__container__formContainer__registerForm"
-          >
+        <div className='authentication__container__formContainer'>
+          <p className='authentication__container__formContainer__registerHeading'>{t("Pannel_registration.letsregister")}</p>
+          <form onSubmit={submitHandler} className='authentication__container__formContainer__registerForm'>
             <div>
               <input
                 type="text"
                 placeholder={`${t("Registration.Firstname")}`}
-                name="first_name"
+                name='first_name'
                 value={user.first_name}
                 onChange={changeHandler}
                 required={true}
@@ -98,15 +87,15 @@ const MemberSignUp = () => {
               <input
                 type="text"
                 placeholder={`${t("Registration.Lastname")}`}
-                name="last_name"
+                name='last_name'
                 value={user.last_name}
                 onChange={changeHandler}
                 required={true}
               />
               <input
                 type="email"
-                placeholder="Email address"
-                name="email"
+                placeholder='Email address'
+                name='email'
                 value={user.email}
                 onChange={changeHandler}
                 required={true}
@@ -115,11 +104,9 @@ const MemberSignUp = () => {
               <input
                 type="text"
                 placeholder={`${t("Pannel_Dashboard_Userprofile.Peselnumber")}`}
-                name="company_pasel_number"
+                name='company_pasel_number'
                 value={user.company_pesel_number}
-                onChange={(e) =>
-                  setUser({ ...user, company_pesel_number: e.target.value })
-                }
+                onChange={(e) => setUser({ ...user, company_pesel_number: e.target.value})}
                 required={true}
               />
             </div>
@@ -128,7 +115,7 @@ const MemberSignUp = () => {
               <input
                 type="password"
                 placeholder={`${t("Registration.setpassword")}`}
-                name="password"
+                name='password'
                 value={user.password}
                 onChange={changeHandler}
                 required={true}
@@ -136,23 +123,22 @@ const MemberSignUp = () => {
               <input
                 type="password"
                 placeholder={`${t("Registration.repeatpassword")}`}
-                name="password_confirmation"
+                name='password_confirmation'
                 value={user.password_confirmation}
                 onChange={changeHandler}
                 required={true}
               />
             </div>
-            <Button
-              className="authentication__container__formContainer__registerForm__registerButton"
-              type="submit"
-            >
-              {t("Pannel_Login.registernow")}
+            <Button className='authentication__container__formContainer__registerForm__registerButton' type="submit">
+            {t("Pannel_Login.registernow")}
+
             </Button>
           </form>
-        </div>
-      </div>
-    </section>
-  );
-};
+        </div >
 
-export default MemberSignUp;
+      </div >
+    </section >
+  )
+}
+
+export default MemberSignUp
