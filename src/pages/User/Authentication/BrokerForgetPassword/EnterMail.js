c1a9b1933caaf087143bd75692c97759caff8f9d
Sameer <sameersheikh6000@gmail.com>
2023-08-14 20:42:34 +0500

    fixed issue on Enter mail forgot password

--- src/pages/User/Authentication/BrokerForgetPassword/EnterMail.js
+++ src/pages/User/Authentication/BrokerForgetPassword/EnterMail.js
@@ -17,35 +17,34 @@ const EnterMail = () => {
   const lang = currentUrl.split("/").pop();
   const { t } = useTranslation();
 
-  useEffect(() => {
-    const currentUrl = window.location.href;
-    let lang = currentUrl.split("/").pop();
-    lang && i18n.changeLanguage(lang === "pl" ? lang : "en");
-  }, []);
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setIsLoading(true);
+    // setIsLoading(true);
     const response = await forgotPassword(email);
     if (response?.status < 300) {
       setSuccessMessage(response?.message);
-      setIsLoading(false);
+      // setIsLoading(false);
       setTimeout(() => {
         navigate(`/signin/${lang === "pl" ? lang : "en"}`);
       }, 3000);
-      setEmail("");
+      // setEmail("");
     } else if (response?.status > 300) {
-      setIsLoading(false);
+      // setIsLoading(false);
       setAlertMessage(response?.message);
       setTimeout(() => {
         setAlertMessage("");
       }, 3000);
     }
   };
+  
+  useEffect(() => {
+    const currentUrl = window.location.href;
+    let lang = currentUrl.split("/").pop();
+    lang && i18n.changeLanguage(lang === "pl" ? lang : "en");
+  }, []);
+  
   return (
     <>
-      {/* <SuccessMessage successMessage={successMessage} />
-      <AlertMessage alertMessage={alertMessage} /> */}
       <section className="authentication">
         <div className="authentication__container">
           <div className="authentication__container__imageBox">
@@ -56,11 +55,6 @@ const EnterMail = () => {
                 src={require("../../../../assets/Signin-logo.png")}
                 alt=""
               />
-              {/* <img
-              className="authentication__container__imageBox__top__flag"
-              src={require("../../../../assets/Signin-flag.png")}
-              alt=""
-            /> */}
             </div>
 
             <div
@@ -93,10 +87,6 @@ const EnterMail = () => {
             >
               {t("Forget_psw.enter_email")}{" "}
             </p>
-            {/* <div className="supportView__header__iconBox__forget">
-            <MailOutlinedIcon className="supportView__header__iconBox__icon__forget" />
-            <p>Email Address</p>
-          </div> */}
             <form
               onSubmit={handleSubmit}
               className="authentication__container__formContainer__form__forget"
@@ -133,7 +123,7 @@ const EnterMail = () => {
                 {!isLoading ? (
                   t("Forget_psw.continue")
                 ) : (
-                  <CircularProgress style={{ color: "white" }} />
+                  <CircularProgress style={{ color: "white", width: '20px', height: '20px' }} />
                 )}
               </Button>
             </form>
